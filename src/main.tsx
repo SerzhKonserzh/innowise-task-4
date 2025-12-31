b59357ac13a3289b3f7ca356b0bdee436b0c3bff
Sergey <Serzh-2001BTR@mail.ru>
2025-12-31 18:31:36 +0300

    feat: add emotion theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,30 +1,33 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { routeTree } from './routes/routeTree.gen.ts'
+import { routeTree } from './routes/routeTree.gen.ts';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './theme.tsx';
 
-const router = createRouter({ routeTree })
+const router = createRouter({ routeTree });
 
 declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
+	interface Register {
+		router: typeof router;
+	}
 }
 
 const queryClient = new QueryClient({
 	defaultOptions: {
 		queries: {
-			staleTime: 10 * 1000,
+			staleTime: 10 * 1000
 		}
 	}
-}
-);
+});
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router} />
-		</QueryClientProvider>
+		<ThemeProvider theme={theme}>
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
+		</ThemeProvider>
 	</StrictMode>
 );
